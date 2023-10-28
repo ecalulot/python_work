370e94449b2078aa05cf95f68931e08142e4426f
Efren Calulot <37412760+ecalulot@users.noreply.github.com>
2023-10-28 10:59:57 -0600

    continue learning python

---
+++ ch5_p88_exercieses.py
@@ -0,0 +1,9 @@
+#5-8. Hello Admin
+more_users = ['Alice', 'Adam', 'Eve', 'Admin', 'Bob', 'Zeus']
+for user in more_users:
+    if user == 'Admin':
+        print("Hello Admin, would you like to see a status report?\n")
+    else:
+        print(f"Salutations {user}, thank you for logging in again.\n")
+
+#5-9. No Users
