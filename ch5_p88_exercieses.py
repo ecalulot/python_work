9eb4c3b4058ec8f96e9a1c7e499984ca8e4273e2
Efren Calulot <37412760+ecalulot@users.noreply.github.com>
2023-10-29 15:16:08 -0600

    end Ch5 exercise p.88

--- ch5_p88_exercieses.py
+++ ch5_p88_exercieses.py
@@ -1,4 +1,4 @@
-#5-8. Hello Admin
+print('# 5-8. Hello Admin and # 5-9 No Users\n')
 more_users = ['Alice', 'Adam', 'Eve', 'Hecate', 'Admin', 'Bob', 'Zeus']
 
 # more_users = [] # Empty array or list to test 5-9
@@ -21,12 +21,32 @@ else:
 # else:
 #     print('We need to find more users!')
 
-#5-10 Checking Usernames
+print('# 5-10 Checking Usernames\n')
 current_users = ['Cronus', 'Rhea', 'Atlas', 'Prometheus', 'Helios']
-new_users = ['Oceanus', 'Tethys', 'Phoebe', 'Atlas', 'Theia', 'Selene', 'Rhea']
+new_users = ['Oceanus', 'Tethys', 'Phoebe', 'Atlas', 'Theia', 'Selene', 
+             'Rhea', 'HELIOS', 'cronUS']
+
+# test for case insensitivity
+icase_current_users = [] # initialize an empty array
+for cur_user in current_users:
+    # lowercase each user and add to the empty list array
+    icase_current_users.append(cur_user.lower()) 
+#print(icase_current_users)
+
 for user in new_users:
-    if user in current_users:
+    if user.lower() in icase_current_users:
         print(f"The {user} name is unavailable. Enter a new username.\n")
     else:
         print(f"The username {user} is available.\n")
 
+print('# 5-11 Ordinal Numbers\n')
+ord_nums = list(range(1, 10))
+for num in ord_nums:
+    if num == 1:
+        print('1st')
+    elif num == 2:
+        print('2nd')
+    elif num == 3:
+        print('3rd')
+    else:
+        print(str(num) + 'th')
