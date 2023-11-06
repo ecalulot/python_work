0185c9540ef8394d1da5320d8c03f656685014f1
Efren Calulot <37412760+ecalulot@users.noreply.github.com>
2023-11-06 10:52:55 -0700

    more practice on dictionaries

---
+++ ch6-exercises-p98.py
@@ -0,0 +1,9 @@
+# 6-1 Person (p. 98)
+person = {'first_name': 'Dante', 'last_name': 'Alighieri', 'city': 'Florence'}
+print(person['first_name'])
+print(person['last_name'])
+print(person['city'])
+
+
+# 6-2 Favorite Numbers
+
